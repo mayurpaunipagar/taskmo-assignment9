88120905c2980645d99d8fd509373f1979a7aeae
Mayur Paunipagar <mayur.paunipagar@taskmo.com>
2021-05-30 18:16:19 +0530

    ui-complete

--- src/App.js
+++ src/App.js
@@ -7,23 +7,28 @@ function App() {
       <div className="myPhone">
         <img src={window.location.origin+"/images/LEAD.svg"} alt="lead_logo" className="leadLogo"/>
         <div className="welcome">Welcome to TASKMO</div>
-        <div >Thank you for showing the interest</div>
+        <div className="thanks" >Thank you for showing the interest</div>
         <div className="myForm">
-          <input type="text" className="myInput" placeholder="Full Name"/>
-          <input type="text" className="myInput" placeholder="Contact Number"/>
-          <input type="text" className="myInput" placeholder="Whatsapp Number"/>
-          <input type="text" className="myInput" placeholder="Gender"/>
+          <div className="formText">Please fill the below details</div>
+          <input type="text" className="myInput" placeholder="Enter your name"/>
+          <input type="text" className="myInput" placeholder="Enter contact number"/>
+          <input type="text" className="myInput" placeholder="Enter whatsapp number"/>
           <select type="text" className="myInput selectInput" >
-          <option className="selectPlaceholder" value="" disabled selected hidden>State</option>
+          <option className="selectPlaceholder" value="" disabled selected hidden>Select your gender</option>
+          </select>
+          <select type="text" className="myInput selectInput" >
+          <option className="selectPlaceholder" value="" disabled selected hidden>Choose your State</option>
           </select>
           <select type="text" className="myInput selectInput">
-          <option className="selectPlaceholder" value="" disabled selected hidden>City</option>
+          <option className="selectPlaceholder" value="" disabled selected hidden>Choose your City</option>
           </select>
-          <input type="text" className="myInput" placeholder="Pincode"/>
+          <input type="text" className="myInput" placeholder="Enter area pincode"/>
           <select type="text" className="myInput selectInput">
-          <option className="selectPlaceholder" value="" disabled selected hidden>Job Role</option>
+          <option className="selectPlaceholder" value="" disabled selected hidden>Select job role</option>
           </select>
-          <input type="text" className="myInput" placeholder="Languages"/>
+          <select type="text" className="myInput selectInput" >
+          <option className="selectPlaceholder" value="" disabled selected hidden>Select languages you know</option>
+          </select>
           <select type="text" className="myInput selectInput" >
           <option className="selectPlaceholder" value="" disabled selected hidden>Rate your English communication</option>
           </select>
