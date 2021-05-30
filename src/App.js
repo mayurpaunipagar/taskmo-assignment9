b46ff81011291686478cb0d5875d3929d47f2029
Mayur Paunipagar <mayur.paunipagar@taskmo.com>
2021-05-30 17:14:36 +0530

    ui update1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="myPhone">
+        <img src={window.location.origin+"/images/LEAD.svg"} alt="lead_logo" className="leadLogo"/>
+        <div className="welcome">Welcome to TASKMO</div>
+        <div >Thank you for showing the interest</div>
+        <div className="myForm">
+          <input type="text" className="myInput" placeholder="Full Name"/>
+          <input type="text" className="myInput" placeholder="Contact Number"/>
+          <input type="text" className="myInput" placeholder="Whatsapp Number"/>
+          <input type="text" className="myInput" placeholder="Gender"/>
+          <select type="text" className="myInput selectInput" >
+          <option className="selectPlaceholder" value="" disabled selected hidden>State</option>
+          </select>
+          <select type="text" className="myInput selectInput">
+          <option className="selectPlaceholder" value="" disabled selected hidden>City</option>
+          </select>
+          <input type="text" className="myInput" placeholder="Pincode"/>
+          <select type="text" className="myInput selectInput">
+          <option className="selectPlaceholder" value="" disabled selected hidden>Job Role</option>
+          </select>
+          <input type="text" className="myInput" placeholder="Languages"/>
+          <select type="text" className="myInput selectInput" >
+          <option className="selectPlaceholder" value="" disabled selected hidden>Rate your English communication</option>
+          </select>
+          <div className="submitBtn glow">SUBMIT</div>
+        </div>
+      </div>
     </div>
   );
 }
